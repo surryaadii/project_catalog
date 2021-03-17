c18783e69b9db0e1d9b14b4f53d72fc4ec999cfe
suryadi sukisna <surryaadii@yahoo.com>
2021-03-17 20:12:52 +0700

    added image service and layput footer top-bar

---
+++ resources/views/admin/layouts/footer.blade.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>Version</b> 2.4.13
+    </div>
+    <strong>Copyright &copy; {{ date('Y') }} <a href="#"><i class="fa fa-angle-up"></i></a></strong>
+  </footer>
