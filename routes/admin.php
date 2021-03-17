1ad1d1f81a72fb5474a5adbcaf35ad836f78d820
suryadi sukisna <surryaadii@yahoo.com>
2021-03-17 19:16:27 +0700

    initial commit

---
+++ routes/admin.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+/*
+|--------------------------------------------------------------------------
+| Admin Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "admin" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', 'Admin\\DashboardController@index')->name('admin.dashboard');
